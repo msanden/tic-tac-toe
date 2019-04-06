9de513585796f58272d558707260f5922c44f316
Manuel Sande <mnlsande22@gmail.com>
2019-04-06 07:07:49 -0400

    Add tie_game method

--- ticTacToe.py
+++ ticTacToe.py
@@ -8,32 +8,37 @@ class Board():
          self.cells = [" "," "," "," "," "," "," "," "," "," "]
 
     def display(self):
-        print(f" {self.cells[1]}|{self.cells[2]}|{self.cells[3]}")
-        print("-------")
-        print(f" {self.cells[4]}|{self.cells[5]}|{self.cells[6]}")
-        print("-------")
-        print(f" {self.cells[7]}|{self.cells[8]}|{self.cells[9]}")
+        print(f"""
+                {self.cells[1]}|{self.cells[2]}|{self.cells[3]}
+               -------
+                {self.cells[4]}|{self.cells[5]}|{self.cells[6]}
+               -------
+                {self.cells[7]}|{self.cells[8]}|{self.cells[9]}
+        """)
 
     def update_box(self, box_no, player):
         if self.cells[box_no] == " ":
             self.cells[box_no] = player
 
     def to_win(self, player):
-        if self.cells[1]==player and self.cells[2]==player and self.cells[3]==player:
+        if self.cells[1]==player and self.cells[2]==player and self.cells[3]==player or \
+        (self.cells[4]==player and self.cells[5]==player and self.cells[6]==player) or \
+        (self.cells[7]==player and self.cells[8]==player and self.cells[9]==player) or \
+        (self.cells[1]==player and self.cells[4]==player and self.cells[7]==player) or \
+        (self.cells[2]==player and self.cells[5]==player and self.cells[8]==player) or \
+        (self.cells[3]==player and self.cells[6]==player and self.cells[9]==player) or \
+        (self.cells[1]==player and self.cells[5]==player and self.cells[9]==player) or \
+        (self.cells[3]==player and self.cells[5]==player and self.cells[7]==player):
             return True
-        if self.cells[4]==player and self.cells[5]==player and self.cells[6]==player:
-            return True
-        if self.cells[7]==player and self.cells[8]==player and self.cells[9]==player:
-            return True
-        if self.cells[1]==player and self.cells[4]==player and self.cells[7]==player:
-            return True
-        if self.cells[2]==player and self.cells[5]==player and self.cells[8]==player:
-            return True
-        if self.cells[3]==player and self.cells[6]==player and self.cells[9]==player:
-            return True
-        if self.cells[1]==player and self.cells[5]==player and self.cells[9]==player:
-            return True
-        if self.cells[3]==player and self.cells[5]==player and self.cells[7]==player:
+        else:
+            return False
+
+    def tie_game(self):
+        usedCell = 0
+        for cell in self.cells:
+            if cell != " ":
+                usedCell = usedCell + 1
+        if usedCell == 9:
             return True
 
     def reset(self):
@@ -42,7 +47,12 @@ class Board():
 board = Board()
 
 def print_header():
-    print("Welcome to tic-tac-toe\n")
+    print("""
+    Welcome to tic-tac-toe!
+    Select a cell between 1-9 to play. 1|2|3
+                                       4|5|6
+                                       7|8|9
+    """)
 
 def refresh_screen():
     os.system("clear")
@@ -52,13 +62,22 @@ def refresh_screen():
 while True:
     refresh_screen()
     #get player X's input
-    x_input = int(input("\n (Player X) Choose a box between 1-9. > "))
+    x_input = int(input("\n (Player X) Select a cell between 1-9. > "))
     board.update_box(x_input, "x")
     refresh_screen()
 
     if board.to_win("x"):
         print("\n X wins! \n")
         new_game = input("Would you like to play again? (Y/N) >").upper()
+        if new_game == "Y":
+            board.reset()
+            continue
+        else:
+            break
+
+    if board.tie_game():
+        print("It's a tie!")
+        new_game = input("Restart Game? (Y/N) >").upper()
         if new_game == "Y":
             board.reset()
             continue
@@ -72,6 +91,15 @@ while True:
 
     if board.to_win("o"):
         print("\n O wins! \n")
+        new_game = input("Restart Game? (Y/N) >").upper()
+        if new_game == "Y":
+            board.reset()
+            continue
+        else:
+            break
+
+    if board.tie_game():
+        print("It's a tie!")
         new_game = input("Would you like to play again? (Y/N) >").upper()
         if new_game == "Y":
             board.reset()
